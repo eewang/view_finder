ef0f4dd1f2e119c516c01de66880fc933c5e2ead
Eugene Wang <eugene.wang.77@gmail.com>
2013-03-23 13:19:22 -0400

    fixed typos

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :instagram, ENV['9fd83d36f6094ef59fbaaadd8febab43'], ENV['8170b00870e548f6b2dc505660927075']
+  provider :instagram, ENV['#'], ENV['#']
 end
