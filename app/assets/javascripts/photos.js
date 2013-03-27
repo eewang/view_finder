38408b2eb74ebdc51ad1db5f07b8e8dc6f20469e
Erin L <email.erin@gmail.com>
2013-03-27 13:13:17 -0400

    Revert "fixed typos"
    This reverts commit df8dedee54922fb473375fd6838a539c8ece3835.

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -12,21 +12,21 @@ $(document).ready(function(){
   // google.maps.event.trigger(map, "resize");
 
   $(".btn-primary").click(function(){
-    // event.preventDefault();
-    // console.log("hello");
+    event.preventDefault();
+    console.log("hello");
   })
 
-  // google.maps.event.addListenerOnce(map, 'idle', function(){
-  //     $(window).resize();
-  //     map.setCenter(yourCoordinates);
-  // });
+  google.maps.event.addListenerOnce(map, 'idle', function(){
+      $(window).resize();
+      map.setCenter(yourCoordinates);
+  });
 
-  // $(window).resize(function() {
-  //     if ($("#map_canvas").children().length > 0){    
-  //         resize();
-  //         if (map)
-  //         google.maps.event.trigger(map, 'resize');
-  //     }});
+  $(window).resize(function() {
+      if ($("#map_canvas").children().length > 0){    
+          resize();
+          if (map)
+          google.maps.event.trigger(map, 'resize');
+      }});
 
 
 })
