b23ce8be77c10d06f7c160a41b7c1f69e13f169f
Eugene Wang <eugene.wang.77@gmail.com>
2013-03-30 16:29:47 -0400

    more fixes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,2 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :instagram, ENV['INSTAGRAM_APP_ID'], ENV['INSTAGRAM_SECRET']
-
-# Instagram.configure do |config|
-#   config.client_id = "9fd83d36f6094ef59fbaaadd8febab43"
-#   config.access_token = "305166995.9fd83d3.347d159cd5a94d638d7fc1df970c88b4"
-# end
-
-# OmniAuth.config.on_failure = UsersController.action(:oauth_failure)
