2bf27798415b173ecace1bba3d3b32a37770e99e
Eugene Wang <eugene.wang.77@gmail.com>
2013-03-30 16:28:43 -0400

    fixed api credentials

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :instagram, "d3e6c95d842c4e91b659b4037769d729", "95a936548ff54b6ab2a28bda596841ee"
-end
+  provider :instagram, ENV['INSTAGRAM_APP_ID'], ENV['INSTAGRAM_SECRET']
 
 # Instagram.configure do |config|
 #   config.client_id = "9fd83d36f6094ef59fbaaadd8febab43"
