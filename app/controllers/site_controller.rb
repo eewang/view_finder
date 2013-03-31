90a1b176b431a26d876aec9e33b17bc4bcf64a62
Eugene Wang <eugene.wang.77@gmail.com>
2013-03-31 16:50:08 -0400

    cleaned up home page

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -6,19 +6,19 @@ class SiteController < ApplicationController
 
 		union_square_coordinates = PhotosController::LOCATION_GAMES[:union_square][:coordinates]
 		union_square_radius = PhotosController::LOCATION_GAMES[:union_square][:radius]
-		union_square_size = 3
+		union_square_size = 10
 
 		thirty_rock_coordinates = PhotosController::LOCATION_GAMES[:thirty_rock][:coordinates]
 		thirty_rock_radius = PhotosController::LOCATION_GAMES[:thirty_rock][:radius]
-		thirty_rock_size = 3
+		thirty_rock_size = 10
 
 		central_park_coordinates = PhotosController::LOCATION_GAMES[:central_park][:coordinates]
 		central_park_radius = PhotosController::LOCATION_GAMES[:central_park][:radius]
-		central_park_size = 3
+		central_park_size = 10
 
 		@union_square = Photo.game_photos_random(union_square_coordinates, union_square_radius, user, union_square_size)
+    @central_park = Photo.game_photos_random(central_park_coordinates, central_park_radius, user, central_park_size)
 		@thirty_rock = Photo.game_photos_random(thirty_rock_coordinates, thirty_rock_radius, user, thirty_rock_size)
-		@central_park = Photo.game_photos_random(central_park_coordinates, central_park_radius, user, central_park_size)
 
 		@myfavorite = [@union_square, @central_park, @thirty_rock]
 
