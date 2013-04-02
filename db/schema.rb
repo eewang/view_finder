da1d5032dbe7f4488ecf4e8869de9c62ed008095
Eugene Wang <eugene.wang.77@gmail.com>
2013-04-02 18:42:57 -0400

    rollback migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130402214507) do
+ActiveRecord::Schema.define(:version => 20130402193227) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -64,12 +64,8 @@ ActiveRecord::Schema.define(:version => 20130402214507) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
-    t.string   "instagram_uid"
-    t.string   "instagram_nickname"
-    t.string   "instagram_token"
-    t.string   "instagram_avatar"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
