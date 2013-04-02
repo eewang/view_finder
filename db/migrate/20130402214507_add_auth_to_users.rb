fd7bf78ca518633ee28cfe2181b51493f93cfeff
Eugene Wang <eugene.wang.77@gmail.com>
2013-04-02 18:43:24 -0400

    deleted migration to add auth to users table

--- db/migrate/20130402214507_add_auth_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddAuthToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :instagram_uid, :string
-    add_column :users, :instagram_nickname, :string
-    add_column :users, :instagram_token, :string
-    add_column :users, :instagram_avatar, :string
-  end
-end
