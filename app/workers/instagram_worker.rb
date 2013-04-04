1d3f8987f686134073fcfa2bdc2b56431f0f615b
Eugene Wang <eugene.wang.77@gmail.com>
2013-04-04 16:18:23 -0400

    added search by #viewfinder or #vfyw to instagram worker

--- app/workers/instagram_worker.rb
+++ app/workers/instagram_worker.rb
@@ -5,6 +5,8 @@ class InstagramWorker
   def perform(coordinates)
     hash = {:lat => coordinates[0], :lon => coordinates[1]}
     Photo.instagram_media_search(hash)
+    Photo.instagram_tag_recent_media(:tag => "viewfinder")
+    Photo.instagram_tag_recent_media(:tag => "vfyw")
   end
 
 end
