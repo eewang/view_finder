c9d30af938ffcfe809339259bd74fa010144d3ea
Eugene Wang <eugene.wang.77@gmail.com>
2013-04-10 22:42:34 -0400

    cleaned up photo index view with helpers

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,2 +1,26 @@
 module PhotosHelper
+
+  def route(photo, user)
+    photo.guessed_by?(user) ? "guesses" : "photos"
+  end
+
+  def link(photo, user)
+    photo.guessed_by?(user) ? photo.guesses.where(:user_id => user.id).first.id : photo.game
+  end
+
+  def method(photo, user)
+    photo.guessed_by?(user) ? "GET" : "POST"
+  end
+
+  def view_play(photo, user)
+    if photo.guessed_by?(user)
+      link_to("VIEW", 
+              "/#{route(photo, user)}/#{link(photo, user)}",
+              :class => "styled-button-5 view_play_button")
+    else
+      submit_tag("PLAY",
+                :class => "styled-button-5 view_play_button")
+    end
+  end
+
 end
