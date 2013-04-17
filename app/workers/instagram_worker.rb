307f5e171511f83ba4f65f9491d15a3ccba82f6e
Eugene Wang <eugene.wang.77@gmail.com>
2013-04-17 16:04:07 -0400

    turned off sidekiq background task

--- app/workers/instagram_worker.rb
+++ app/workers/instagram_worker.rb
@@ -4,9 +4,9 @@ class InstagramWorker
   
   def perform(coordinates)
     hash = {:lat => coordinates[0], :lon => coordinates[1]}
-    Photo.instagram_media_search(hash)
-    Photo.instagram_tag_recent_media(:tag => "viewfinder")
-    Photo.instagram_tag_recent_media(:tag => "vfyw")
+    # Photo.instagram_media_search(hash)
+    # Photo.instagram_tag_recent_media(:tag => "viewfinder")
+    # Photo.instagram_tag_recent_media(:tag => "vfyw")
   end
 
 end
